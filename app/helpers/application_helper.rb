0c27808d784b37492eb9a294367eaf03b8e9e8b1
dilipcode <drsar8@gmail.com>
2016-05-18 14:53:42 +0530

    Added flash message alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+
+  def flash_messages_tag
+    #     <% flash.each do |fl| %>
+    #   <div class="alert alert-<%=fl[0] %> alert-dismissible" role="alert">  
+    #     <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    #     <%= fl[1] %>
+    #   </div>  
+    # <% end %>
+    #   end
+    
+    str = " "
+    flash.each do |fl|
+      fl[0] = "success" if fl[0] == "notice" 
+      fl[0] = "danger" if fl[0] == "error" || fl[0] == "alert"
+      str+= content_tag(:div, class: "alert alert-#{fl[0]} alert-dismissible") do 
+              content_tag(:button, class: "close", data: {dismiss: "alert"}) do
+                content_tag(:span, raw("&times;")) 
+              end + fl[1]
+            end      
+    end
+    return raw(str)
+  end
 end
