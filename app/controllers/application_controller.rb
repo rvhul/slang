847afa69e6470a454b64bca8f74782b7ea6d9759
dilipcode <drsar8@gmail.com>
2016-05-20 13:32:43 +0530

    Customized Devise signup view and user model to accomodate more user details

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  # configuration for devise controller
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+        def configure_permitted_parameters
+            # params.require(:sign_up).permit(:name, :is_female, :date_of_birth, :email, :password, :password_confirmation)
+            devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :is_female, :date_of_birth, :email, :password, :password_confirmation) }
+            devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :is_female, :date_of_birth) }
+            # params.require(:account_update).permit(:name, :is_female, :date_of_birth, :email, :password, :current_password )
+        end
 end
